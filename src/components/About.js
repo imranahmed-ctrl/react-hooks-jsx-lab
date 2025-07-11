e546b8a2bd2525bb63a6ae62bc963b207cc29fc4
imran <otcimran16@gmail.com>
2025-07-11 20:51:03 +0300

    commits

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">About
+  <h2>"About Me"</h2>
+  <p>I love Manchester</p>
+  <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this"></img>
+  </div>;
 }
 
 export default About;
